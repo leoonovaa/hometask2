620c1461d1a3793ba20d0d5c05c5ef489d479a4b
leoonovaa <144064947+leoonovaa@users.noreply.github.com>
2025-11-20 12:10:56 +0200

    userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -2,6 +2,9 @@
 
 yum update -y
 yum install -y mc git vim httpd
+yum install -y amazon-ssm-agent
 
 systemctl enable httpd
 systemctl start httpd
+systemctl enable amazon-ssm-agent
+systemctl start amazon-ssm-agent
